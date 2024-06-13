351f8182bc23472a1ef782c5d6e308994706b519
Udana Nimsara <nimsaragcu.22@uom.lk>
2024-06-13 21:26:33 +0530

    Stop tracking config.js

--- src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    BOT_TOKEN: 'Your Bot Token Here', // bot`s token. You can get that in @BotFather
-    GROUP_ID: 'Your Group ID Here' // // Group ID of the event group. You can get it using the /grpid command after adding the bot to the group.
-  }
